d1f075dc0e7cf809d6daabe6ae21001fef27d1ec
buyeolim <buyeolim@gmail.com>
2023-05-31 23:50:16 +0900

    [feat] firebase 로그인 확인 후 홈 또는 로그인 페이지 이동

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { auth } from './firebase/firebase';
+import SignIn from './pages/SignIn/SignIn';
+import Home from './pages/Home/Home';
 
 function App() {
+  const [isInit, setIsInit] = useState(false); // firebase 초기화 확인
+  const [isLogin, setIsLogin] = useState(false); // 현재 유저로그인(O: true, X: false)
+
+  useEffect(() => {
+    // auth 상태가 변경(로그인 또는 로그아웃)되면 수행
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLogin(true); // 로그인 상태
+      } else {
+        setIsLogin(false); // 로그아웃 상태
+      }
+
+      setIsInit(true); // firebase 초기화 완료
+    });
+  }, []);
+
   return (
-    <div>
-      hi
-    </div>
+    <>
+      {isInit ? (
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={isLogin ? <Home /> : <Navigate replace to="/login" />}
+            />
+            <Route path="/login" element={<SignIn isLogin={isLogin} />} />
+          </Routes>
+        </BrowserRouter>
+      ) : (
+        'Loading...'
+      )}
+    </>
   );
 }
 
---
+++ src/pages/SignIn/SignIn.jsx
@@ -0,0 +1,5 @@
+function SignIn() {
+  return <div>로그인 페이지</div>;
+}
+
+export default SignIn;
