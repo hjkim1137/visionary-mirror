6ec3e3ce0d03d8094c64272513ff239cd812c625
hyunjung <paintover2023@gmail.com>
2023-06-01 10:55:25 +0900

    [feat] dev 브랜치 폴더구조 반영

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Header from './pages/Header';
+import GetSampleBoard from './pages/GetSampleBoard';
+import MakeBoardName from './pages/MakeBoardName';
+import MyVisionBoard from './pages/MyVisionBoard';
+import MyVisionBoardGrid from './pages/MyVisionBoardGrid';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import VisionBoardGrid from './pages/VisionBoardGrid ';
 
 function App() {
   return (
-    <div>
-      hi
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Header />} />
+        <Route path="/getsampleboard" element={<GetSampleBoard />} />
+        <Route path="/makeBoardName" element={<MakeBoardName />} />
+        <Route path="/myvisionboard" element={<MyVisionBoard />} />
+        <Route path="/myvisionboardgrid/:id" element={<MyVisionBoardGrid />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/register" element={<SignUp />} />
+        <Route path="/visionboardgrid" element={<VisionBoardGrid />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
