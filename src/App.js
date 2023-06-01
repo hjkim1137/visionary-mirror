b1e75d77f921a0f80b05ef2a9a55e6f6e1493c15
lookethis <113021028+lookethis@users.noreply.github.com>
2023-06-01 13:50:18 +0900

    [rename] dev pull 전 폴더구조 변경

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes } from 'react-router-dom';
-import Header from './Layout/Header/Header';
+import Header from './pages/Header';
 // import Home from "./Home";
 
 function App() {
