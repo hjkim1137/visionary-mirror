4ee0b5662176fce726d1a0db270f8d8da999545f
buyeolim <buyeolim@gmail.com>
2023-06-01 11:10:32 +0900

    [feat] 로그인, 회원가입 페이지 구현

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
 
-function SignIn() {
+function SignUp() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  /** 로그인 기능 수행 */
-  const onSubmit = async (e) => {
+  /** 회원가입 기능 수행 */
+  const onSubmit = (e) => {
     e.preventDefault();
   };
   /** 필드 입력시 해당 값 갱신 */
@@ -15,16 +14,10 @@ function SignIn() {
       target: { name, value },
     } = e;
   };
-  /** 다른 계정으로 가입 및 로그인 */
-  const onClick = async (e) => {
-    const {
-      target: { name },
-    } = e;
-  };
 
   return (
     <>
-      <div>로그인</div>
+      <div>회원가입</div>
       <form onSubmit={onSubmit}>
         <div>
           <input
@@ -47,21 +40,11 @@ function SignIn() {
           />
         </div>
         <div>
-          <input type="submit" value="로그인" />
+          <input type="submit" value="회원가입" />
         </div>
       </form>
-      <div>
-        <Link to="/register">
-          <button>회원가입</button>
-        </Link>
-      </div>
-      <div>
-        <button name="google" onClick={onClick}>
-          구글 아이디로 시작
-        </button>
-      </div>
     </>
   );
 }
 
-export default SignIn;
+export default SignUp;
