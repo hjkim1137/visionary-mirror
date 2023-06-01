7d04fe29cb796339d30516582dcd2102f26d3a90
hyunjung <paintover2023@gmail.com>
2023-06-01 18:45:18 +0900

    [feat] 일방향 슬라이드로 변경 및 자동넘김 기능 추가

--- src/components/MyVisionBoard/BoardCollection.jsx
+++ src/components/MyVisionBoard/BoardCollection.jsx
@@ -11,7 +11,7 @@ function BoardCollection() {
   const [collectionImg, setCollectionImg] = useState([]);
   const [collectionTitle, setCollectionTitle] = useState([]);
   const [collectionId, setCollectionId] = useState([]);
-  const [totalSlides, setTotalSlides] = useState(0);
+  // const [totalSlides, setTotalSlides] = useState(0);
 
   useEffect(() => {
     try {
@@ -24,9 +24,6 @@ function BoardCollection() {
           setCollectionImg(imgUrls);
           setCollectionTitle(imgTitles);
           setCollectionId(imgIds);
-        })
-        .catch((err) => {
-          console.error('에러 발생:', err);
         });
     } catch (err) {
       console.error('비동기 처리 중 오류가 발생했습니다:', err);
@@ -34,13 +31,13 @@ function BoardCollection() {
   }, []);
 
   // 슬라이드 개수 가져오기(4 or 8)
-  useEffect(() => {
-    fetch(mockAPI)
-      .then((response) => response.json())
-      .then((slides) => {
-        setTotalSlides(slides.length);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch(mockAPI)
+  //     .then((response) => response.json())
+  //     .then((slides) => {
+  //       setTotalSlides(slides.length);
+  //     });
+  // }, []);
 
   // 컬렉션 상세보기 페이지 넘어가기
   const navigate = useNavigate();
@@ -62,86 +59,57 @@ function BoardCollection() {
 
   //슬라이드 관련
   const slideRef = useRef(null);
-  const [index, setIndex] = useState(0); // 슬라이드 인덱스
+  const [index, setIndex] = useState(0); // 표시되고 있는 슬라이드 인덱스
   const [isSlide, setIsSlide] = useState(false); // 슬라이드 중 여부 체크, 여러번 빠르게 클릭 못하게 하는 역할
   const [x, setX] = useState(0); // 슬라이드 애니메이션 효과를 주기위한 x만큼 이동
 
-  //버튼 클릭으로 슬라이드 넘기기
-  const [isClick, setIsClick] = useState(false); // 드래그를 시작하는지 체크해줍니다.
-  const [mouseDownClientX, setMouseDownClientX] = useState(0); // 마우스를 클릭한 지점의 x 좌표를 저장
-  const [mouseUpClientX, setMouseUpClientX] = useState(0); // 마우스를 땐 지점의 x 좌표를 저장
+  // 자동으로 슬라이드 넘기기 기능
+  useEffect(() => {
+    let autoSlide;
 
-  //오른쪽 버튼 함수
-  // const increaseClick = async () => {
-  //   if (isSlide) {
-  //     return;
-  //   }
-  //   setX(-56);
-  //   setIsSlide(true);
+    if (index !== collectionImg.length - 1) {
+      // 마지막 슬라이드가 아닐 때만 작동
+      autoSlide = setTimeout(() => {
+        increaseClick();
+      }, 2500); //
+    }
+    // Component가 unmount 되거나 업데이트 되기 전에 타이머를 정리
+    return () => {
+      clearTimeout(autoSlide);
+    };
+  }, [index, collectionImg.length]); // 의존성 배열에 index, collectionImg.length 추가
 
-  //   await setTimeout(() => {
-  //     setIndex((prev) => (prev === 7 ? 0 : prev + 1));
-  //     setX(0);
-  //     setIsSlide(false);
-  //   }, 500);
-  // };
-
-  //2차 시도 - 오른쪽 버튼 함수
+  // 오른쪽 버튼 함수-일방향
   const increaseClick = async () => {
-    if (isSlide) {
+    if (isSlide || index === collectionImg.length - 1) {
+      // 마지막 슬라이드일 경우 이동을 멈춤
       return;
     }
     setX(-56);
     setIsSlide(true);
 
     await setTimeout(() => {
-      setIndex((prev) => (prev + 1) % collectionImg.length); // 변경된 부분
+      setIndex((prev) => prev + 1);
       setX(0);
       setIsSlide(false);
     }, 500);
   };
 
-  //왼쪽 버튼 함수
-  // const decreaseClick = async () => {
-  //   if (isSlide) {
-  //     return;
-  //   }
-  //   setX(+56);
-  //   setIsSlide(true);
-
-  //   await setTimeout(() => {
-  //     setIndex((prev) => (prev === 0 ? 7 : prev - 1));
-  //     setX(0);
-  //     setIsSlide(false);
-  //   }, 500);
-  // };
-
-  //왼쪽 버튼 함수- 2차시도
+  //왼쪽 버튼 함수- 일방향
   const decreaseClick = async () => {
-    if (isSlide) {
+    if (isSlide || index === 0) {
+      // 첫 슬라이드일 경우 이동을 멈춤
       return;
     }
     setX(+56);
     setIsSlide(true);
 
     await setTimeout(() => {
-      setIndex((prev) => (prev - 1) % collectionImg.length); // 변경된 부분
+      setIndex((prev) => prev - 1);
       setX(0);
       setIsSlide(false);
     }, 500);
   };
-
-  // 슬라이드 인덱스 및 개수(이미지 총 8장, 인덱스 0~7)
-  // const morePrevImg = index === 1 ? 7 : index === 0 ? 6 : index - 2; //두 슬라이드 전
-  // const PrevImg = index === 0 ? 7 : index - 1; // 이전 슬라이드 이동(첫 슬라이드에서 왼쪽-> 마지막 슬라이드)
-  // const NextImg = index === 7 ? 0 : index + 1; // 다음 슬라이드 이동(마지막 슬라이드 오른쪽 -> 첫 슬라이드)
-  // const moreNextImg = index === 7 ? 1 : index === 6 ? 0 : index + 2; //두 슬라이드 뒤
-
-  // 캐러셀 원형논리 적용(이미지 총 8장, 인덱스 0~7)
-  // const morePrevImg = (index + 6) % 8; //두 슬라이드 전
-  // const PrevImg = (index + 7) % 8; // 이전 슬라이드 이동(첫 슬라이드에서 왼쪽-> 마지막 슬라이드)
-  // const NextImg = (index + 1) % 8; // 다음 슬라이드 이동(마지막 슬라이드 오른쪽 -> 첫 슬라이드)
-  // const moreNextImg = (index + 2) % 8; //두 슬라이드 뒤
 
   // 캐러셀 원형논리 적용 + 이미지가 8개보다 적을 경우 이미지 인덱스가 실제 이미지 수를 넘어가지 않게 함
   const morePrevImg = (index + collectionImg.length - 2) % collectionImg.length; //두 슬라이드 전
@@ -149,113 +117,28 @@ function BoardCollection() {
   const NextImg = (index + 1) % collectionImg.length; // 다음 슬라이드
   const moreNextImg = (index + 2) % collectionImg.length; //두 슬라이드 뒤
 
-  // 또 수정
-  // const morePrevImg = (index + collectionImg.length - 2) % collectionImg.length; //두 슬라이드 전
-  // const PrevImg = (index + collectionImg.length - 1) % collectionImg.length; // 이전 슬라이드
-  // const CurrentImg = index % collectionImg.length; // 현재 슬라이드
-  // const NextImg = (index + 1) % collectionImg.length; // 다음 슬라이드
-  // const moreNextImg = (index + 2) % collectionImg.length; //두 슬라이드 뒤
-
-  // console.log('슬라이드 인덱스', index);
-
-  // 버튼 클릭으로 슬라이드 넘기기 시작
-  // 마우스 버튼 눌렀을 때
-  const onMouseDown = (event) => {
-    setIsClick(true);
-    setMouseDownClientX(event.pageX);
-    // console.log('슬라이드 ref', slideRef);
-  };
-
-  // 마우스가 범위 밖일 때
-  const onMouseLeave = (event) => {
-    setIsClick(false);
-  };
-
-  // 마우스 버튼 뗐을 때
-  const onMouseUp = (event) => {
-    setIsClick(false);
-    const imgX = mouseDownClientX - mouseUpClientX;
-    // console.log(imgX);
-
-    if (imgX < -100) {
-      slideRef.current.style.transform = `translateX(${imgX}px)`;
-      increaseClick();
-    } else if (imgX > 100) {
-      slideRef.current.style.transform = `translateX(${imgX}px)`;
-      decreaseClick();
-    }
-  };
-
-  // 마우스가 범위 안 일 때
-  const onMouseMove = (event) => {
-    if (!isClick) return;
-    event.preventDefault();
-    setMouseUpClientX(event.pageX);
-    const imgX = mouseDownClientX - mouseUpClientX;
-    if (Math.abs(imgX) > 100) {
-      slideRef.current.style.transform = `translateX(${imgX}px)`;
-    }
-  };
-  // 버튼 클릭으로 슬라이드 넘기기 끝
-
-  // 자동으로 슬라이드 넘기기 기능
-  // useEffect(() => {
-  //   const autoPage = setTimeout(() => {
-  //     setX(-56);
-  //     setIsSlide(true);
-  //     setTimeout(() => {
-  //       setIndex((prev) => (prev === 8 ? 0 : prev + 1));
-  //       setX(0);
-  //       setIsSlide(false);
-  //     }, 500);
-  //   }, 5000);
-  //   return () => {
-  //     clearTimeout(autoPage);
-  //   };
-  // }, [index, isClick]);
-
-  // 자동으로 슬라이드 넘기기 기능- 2차시도
-  // useEffect(() => {
-  //   const autoPage = setTimeout(() => {
-  //     setX(-56);
-  //     setIsSlide(true);
-  //     setTimeout(() => {
-  //       setIndex((prev) => (prev === 7 ? 0 : prev + 1));
-  //       setX(0);
-  //       setIsSlide(false);
-  //     }, 500);
-  //   }, 5000);
-  //   return () => {
-  //     clearTimeout(autoPage);
-  //   };
-  // }, [index, isClick]);
-
-  // 자동으로 슬라이드 넘기기 기능- 3차시도(슬라이드가 4개 또는 8개인 경우)
-  useEffect(() => {
-    const autoPage = setTimeout(() => {
-      setX(-56);
-      setIsSlide(true);
-      setTimeout(() => {
-        setIndex((prev) => (prev + 1) % totalSlides); // mod operator for cycling slides
-        setX(0);
-        setIsSlide(false);
-      }, 500);
-    }, 5000);
-    return () => {
-      clearTimeout(autoPage);
-    };
-  }, [index, isClick, totalSlides]);
-
   // 리턴
   return (
     <div className={styles.wrapper}>
       {/* 왼쪽 버튼 */}
-      <button className={styles.leftButton} onClick={decreaseClick}>
+      <button
+        className={styles.leftButton}
+        onClick={decreaseClick}
+        // 첫번째 페이지 도달 시 버튼 숨김
+        style={{ display: index === 0 ? 'none' : 'block' }}
+      >
         <FiChevronLeft />
       </button>
 
       {/* 오른쪽 버튼 */}
-      <button className={styles.rightButton} onClick={increaseClick}>
+      <button
+        className={styles.rightButton}
+        onClick={increaseClick}
+        // 마지막 페이지 도달 시 버튼 숨김
+        style={{
+          display: index === collectionImg.length - 1 ? 'none' : 'block',
+        }}
+      >
         <FiChevronRight />
       </button>
 
@@ -263,10 +146,6 @@ function BoardCollection() {
       <div
         className={styles.row}
         key={index}
-        onMouseDown={onMouseDown}
-        onMouseUp={onMouseUp}
-        onMouseLeave={onMouseLeave}
-        onMouseMove={onMouseMove}
         ref={slideRef}
         style={{
           transform: `translateX(${x}vw)`,
@@ -285,24 +164,16 @@ function BoardCollection() {
           <img className={styles.priviewImg} src={collectionImg[PrevImg]}></img>
         </div>
 
-        {/* 이미지 섹션 시작 */}
+        {/* 현재 슬라이드 시작 */}
         <div className={styles.imgWrapper}>
-          {/* 한 화면에 세 개 슬라이드 보이게 */}
           <img className={styles.img} src={collectionImg[index]}></img>
 
           {/* 이미지 설명 박스 */}
-          {/* {!isSlide ? ( //슬라이딩 중이 아니면
-            <div className={styles.imgDes}>
-              <div className={styles.title}>{collectionTitle[index]}</div>
-            </div>
-          ) : null} */}
-
-          {/* 이미지 설명 박스 2차 시도  */}
           <div className={styles.imgDes}>
             <div className={styles.title}>{collectionTitle[index]}</div>
           </div>
         </div>
-        {/* 이미지 섹션 끝 */}
+        {/* 현재 슬라이드  끝 */}
 
         {/* 다음 슬라이드에 적용 */}
         <div className={styles.container}>
@@ -319,19 +190,31 @@ function BoardCollection() {
       </div>
 
       {/* 가로 정렬 등 전체 스타일 끝  */}
+
+      {/* 현재 슬라이드 위치 표시 */}
+      <div className={styles.dotWrapper}>
+        {collectionImg.map((_, idx) => (
+          <div
+            key={idx}
+            className={styles.dot + (idx === index ? ' ' + styles.active : '')}
+          ></div>
+        ))}
+      </div>
+
+      {/* 버튼박스 */}
       <div className={styles.buttonBox}>
         <button
           className={styles.deleteButton}
           onClick={() => handleDeleteButtonClick(index)}
         >
-          선택 컬렉션 삭제하기
+          컬렉션 삭제
         </button>
 
         <button
           className={styles.detailButton}
           onClick={() => handleBtnForBoardDetail(collectionId[index])}
         >
-          선택 컬렉션 상세보기
+          컬렉션 상세보기
         </button>
       </div>
     </div>
