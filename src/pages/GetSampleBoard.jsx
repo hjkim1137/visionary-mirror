9b57041f3eed52621763275a8c05769b75795373
hyunjung <paintover2023@gmail.com>
2023-06-02 10:19:43 +0900

    [feat] 이미지 샘플 불러오기 기능 추가

--- src/pages/GetSampleBoard.jsx
+++ src/pages/GetSampleBoard.jsx
@@ -3,6 +3,9 @@
 import React from 'react';
 import styles from '../components/GetSampleBoard/GetSampleBoard.module.scss';
 import { useNavigate } from 'react-router-dom';
+import imageGroup1 from '../components/GetSampleBoard/imageGroup1.png';
+import imageGroup2 from '../components/GetSampleBoard/imageGroup2.png';
+import imageGroup3 from '../components/GetSampleBoard/imageGroup3.png';
 
 function GetSampleBoard() {
   const navigate = useNavigate();
@@ -12,12 +15,44 @@ function GetSampleBoard() {
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.samplePage}>예시 비전보드 샘플들</div>
-      <button className={styles.visionStartBtn} onClick={handleBtnForBoardName}>
-        비전보드 시작하기
-      </button>
-    </div>
+    <>
+      <div className={styles.container}>
+        <div className={styles.imgContainer}>
+          <div className={styles.imageGroup1}>
+            <img
+              src={imageGroup1}
+              alt="이미지 그룹1"
+              className={styles.imgGroupItem}
+            ></img>
+          </div>
+
+          <div className={styles.imageGroup2}>
+            <img
+              src={imageGroup2}
+              alt="이미지 그룹2"
+              className={styles.imgGroupItem}
+            ></img>
+          </div>
+
+          <div className={styles.imageGroup3}>
+            <img
+              src={imageGroup3}
+              alt="이미지 그룹3"
+              className={styles.imgGroupItem}
+            ></img>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.buttonBox}>
+        <button
+          className={styles.visionStartBtn}
+          onClick={handleBtnForBoardName}
+        >
+          Create VisionBoard
+        </button>
+      </div>
+    </>
   );
 }
 
