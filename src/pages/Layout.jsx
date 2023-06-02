5bc2d48975a134eb409a16cec26737f3dc9489cb
hyunjung <paintover2023@gmail.com>
2023-06-02 15:32:57 +0900

    [rename] layout 코드 정렬

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,12 +1,3 @@
-// import React from 'react';
-// import Header from '../components/Header/Header';
-
-// function Layout() {
-//   return <Header></Header>;
-// }
-
-// export default Layout;
-
 import React from 'react';
 import Header from '../components/Header/Header';
 
