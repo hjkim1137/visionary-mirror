666cc12e09d1da8163862b392de525108f76570e
hyunjung <paintover2023@gmail.com>
2023-06-02 14:01:16 +0900

    [rename] layout 감싸기

--- src/pages/MyVisionBoard.jsx
+++ src/pages/MyVisionBoard.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import BoardCollection from '../components/MyVisionBoard/BoardCollection';
+import Layout from './Layout';
 
 function MyVisionBoard() {
-  return <BoardCollection></BoardCollection>;
+  return;
+  <Layout>
+    <BoardCollection></BoardCollection>
+  </Layout>;
 }
 
 export default MyVisionBoard;
