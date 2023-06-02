91d6972339b23c746961aa247f29f424ae8ccad2
hyunjung <paintover2023@gmail.com>
2023-06-02 12:20:03 +0900

    [rename] getsampleboard 컴포넌트 분리

--- src/pages/GetSampleBoard.jsx
+++ src/pages/GetSampleBoard.jsx
@@ -1,59 +1,8 @@
-// branch: feat/#3/get-sample-board
-
 import React from 'react';
-import styles from '../components/GetSampleBoard/GetSampleBoard.module.scss';
-import { useNavigate } from 'react-router-dom';
-import imageGroup1 from '../components/GetSampleBoard/imageGroup1.png';
-import imageGroup2 from '../components/GetSampleBoard/imageGroup2.png';
-import imageGroup3 from '../components/GetSampleBoard/imageGroup3.png';
+import GetSampleBoardCompo from '../components/GetSampleBoard/GetSampleBoardCompo';
 
 function GetSampleBoard() {
-  const navigate = useNavigate();
-
-  const handleBtnForBoardName = () => {
-    navigate('/makeboardname');
-  };
-
-  return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.imgContainer}>
-          <div className={styles.imageGroup1}>
-            <img
-              src={imageGroup1}
-              alt="이미지 그룹1"
-              className={styles.imgGroupItem}
-            ></img>
-          </div>
-
-          <div className={styles.imageGroup2}>
-            <img
-              src={imageGroup2}
-              alt="이미지 그룹2"
-              className={styles.imgGroupItem}
-            ></img>
-          </div>
-
-          <div className={styles.imageGroup3}>
-            <img
-              src={imageGroup3}
-              alt="이미지 그룹3"
-              className={styles.imgGroupItem}
-            ></img>
-          </div>
-        </div>
-      </div>
-
-      <div className={styles.buttonBox}>
-        <button
-          className={styles.visionStartBtn}
-          onClick={handleBtnForBoardName}
-        >
-          Create VisionBoard
-        </button>
-      </div>
-    </>
-  );
+  return <GetSampleBoardCompo></GetSampleBoardCompo>;
 }
 
 export default GetSampleBoard;
