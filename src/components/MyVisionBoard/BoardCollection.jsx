ea3a1bd6a6136adf45449c1858124c93078ccbdc
hyunjung <paintover2023@gmail.com>
2023-06-03 15:52:06 +0900

    [refactor] 코드 리팩터링]

--- src/components/MyVisionBoard/BoardCollection.jsx
+++ src/components/MyVisionBoard/BoardCollection.jsx
@@ -7,11 +7,13 @@ import useDeleteCollection from './DeleteCollection';
 const mockAPI = 'http://localhost:9999/collection';
 
 function BoardCollection() {
-  // db에서 컬렉션 정보 불러오기
-  const [collectionImg, setCollectionImg] = useState([]);
-  const [collectionTitle, setCollectionTitle] = useState([]);
-  const [collectionId, setCollectionId] = useState([]);
+  const [collection, setCollection] = useState({
+    img: [],
+    title: [],
+    id: [],
+  });
 
+  // db에서 컬렉션 정보 불러오기
   useEffect(() => {
     try {
       fetch(mockAPI)
@@ -25,14 +27,17 @@ function BoardCollection() {
           const imgUrls = items.map((item) => item.url);
           const imgTitles = items.map((item) => item.title);
           const imgIds = items.map((item) => item.id);
-          setCollectionImg(imgUrls);
-          setCollectionTitle(imgTitles);
-          setCollectionId(imgIds);
+          setCollection({
+            img: imgUrls,
+            title: imgTitles,
+            id: imgIds,
+          });
         });
     } catch (err) {
       console.error('비동기 처리 중 오류가 발생했습니다:', err);
     }
   }, []);
+
   // 컬렉션 상세보기 페이지 넘어가기
   const navigate = useNavigate();
   const handleBtnForBoardDetail = (id) => {
@@ -40,19 +45,14 @@ function BoardCollection() {
     console.log('상세보기클릭', id);
   };
 
-  // useDeleteCollection 불러오기
+  // useDeleteCollection 커스텀훅 클릭 핸들러
   const handleDeleteButtonClick = useDeleteCollection(
-    collectionTitle,
-    collectionId,
-    collectionImg,
-    setCollectionImg,
-    setCollectionTitle,
-    setCollectionId,
+    collection,
+    setCollection,
     mockAPI
   );
 
   //슬라이드 관련
-  // const slideRef = useRef(null);
   const [index, setIndex] = useState(0); // 표시되고 있는 슬라이드 인덱스
   const [isSlide, setIsSlide] = useState(false); // 슬라이드 중 여부 체크, 여러번 빠르게 클릭 못하게 하는 역할
   const [x, setX] = useState(0); // 슬라이드 애니메이션 효과를 주기위한 x만큼 이동
@@ -61,7 +61,7 @@ function BoardCollection() {
   useEffect(() => {
     let autoSlide;
 
-    if (index !== collectionImg.length - 1) {
+    if (index < collection.img.length - 1) {
       // 마지막 슬라이드가 아닐 때만 작동
       autoSlide = setTimeout(() => {
         increaseClick();
@@ -71,11 +71,11 @@ function BoardCollection() {
     return () => {
       clearTimeout(autoSlide);
     };
-  }, [index, collectionImg.length]); // 의존성 배열에 index, collectionImg.length 추가
+  }, [index, collection.img.length]); // 의존성 배열에 index, collectionImg.length 추가
 
   // 오른쪽 버튼 함수-일방향
   const increaseClick = async () => {
-    if (isSlide || index === collectionImg.length - 1) {
+    if (isSlide || index === collection.length - 1) {
       // 마지막 슬라이드일 경우 이동을 멈춤
       return;
     }
@@ -106,16 +106,17 @@ function BoardCollection() {
   };
 
   // 캐러셀 원형논리 적용 + 이미지가 8개보다 적을 경우 이미지 인덱스가 실제 이미지 수를 넘어가지 않게 함
-  const morePrevImg = (index + collectionImg.length - 2) % collectionImg.length; //두 슬라이드 전
-  const PrevImg = (index + collectionImg.length - 1) % collectionImg.length; // 이전 슬라이드
-  const NextImg = (index + 1) % collectionImg.length; // 다음 슬라이드
-  const moreNextImg = (index + 2) % collectionImg.length; //두 슬라이드 뒤
+  const morePrevImg =
+    (index + collection.img.length - 2) % collection.img.length; //두 슬라이드 전
+  const PrevImg = (index + collection.img.length - 1) % collection.img.length; // 이전 슬라이드
+  const NextImg = (index + 1) % collection.img.length; // 다음 슬라이드
+  const moreNextImg = (index + 2) % collection.img.length; //두 슬라이드 뒤
 
   // 리턴
   return (
     <div className={styles.wrapper}>
       {/* 보유한 컬렉션 없으면 문구 제외 모든 기능 숨김 */}
-      {collectionImg.length === 0 ? (
+      {collection.img.length === 0 ? (
         <div className={styles.noCollection}>
           보유한 컬렉션이 없습니다. 비전보드를 만들어보세요!
         </div>
@@ -137,7 +138,7 @@ function BoardCollection() {
             onClick={increaseClick}
             // 마지막 페이지 도달 시 버튼 숨김
             style={{
-              display: index === collectionImg.length - 1 ? 'none' : 'block',
+              display: index === collection.img.length - 1 ? 'none' : 'block',
             }}
           >
             <FiChevronRight />
@@ -156,7 +157,7 @@ function BoardCollection() {
             <div className={styles.container}>
               <img
                 className={styles.priviewImg}
-                src={collectionImg[morePrevImg]}
+                src={collection.img[morePrevImg]}
               ></img>
             </div>
 
@@ -164,17 +165,17 @@ function BoardCollection() {
             <div className={styles.container}>
               <img
                 className={styles.priviewImg}
-                src={collectionImg[PrevImg]}
+                src={collection.img[PrevImg]}
               ></img>
             </div>
 
             {/* 현재 슬라이드 시작 */}
             <div className={styles.imgWrapper}>
-              <img className={styles.img} src={collectionImg[index]}></img>
+              <img className={styles.img} src={collection.img[index]}></img>
 
               {/* 이미지 설명 박스 */}
               <div className={styles.imgDes}>
-                <div className={styles.title}>{collectionTitle[index]}</div>
+                <div className={styles.title}>{collection.title[index]}</div>
               </div>
             </div>
             {/* 현재 슬라이드  끝 */}
@@ -183,7 +184,7 @@ function BoardCollection() {
             <div className={styles.container}>
               <img
                 className={styles.priviewImg}
-                src={collectionImg[NextImg]}
+                src={collection.img[NextImg]}
               ></img>
             </div>
 
@@ -191,7 +192,7 @@ function BoardCollection() {
             <div className={styles.container}>
               <img
                 className={styles.priviewImg}
-                src={collectionImg[moreNextImg]}
+                src={collection.img[moreNextImg]}
               ></img>
             </div>
           </div>
@@ -200,7 +201,7 @@ function BoardCollection() {
 
           {/* 현재 슬라이드 위치 표시 */}
           <div className={styles.dotWrapper}>
-            {collectionImg.map((_, idx) => (
+            {collection.img.map((_, idx) => (
               <div
                 key={idx}
                 className={
@@ -221,7 +222,7 @@ function BoardCollection() {
 
             <button
               className={styles.detailButton}
-              onClick={() => handleBtnForBoardDetail(collectionId[index])}
+              onClick={() => handleBtnForBoardDetail(collection.id[index])}
             >
               컬렉션 상세보기
             </button>
