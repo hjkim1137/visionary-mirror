6b860697844b4858e037a9ff6052e69c6124cdf3
lookethis <113021028+lookethis@users.noreply.github.com>
2023-06-09 00:10:32 +0900

    [feat] SignUpCompo유효성 검사 적용함, AccountsCompo 구현 중

--- src/components/SignUp/SignUpCompo.jsx
+++ src/components/SignUp/SignUpCompo.jsx
@@ -1,19 +1,78 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../firebase/firebase';
 import styles from './SignUpCompo.module.scss';
 
 function SignUpCompo() {
+  const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
-  // 로그인 되어있으면 홈('/')으로 이동
+  const [usernameError, setUsernameError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [confirmPasswordError, setConfirmPasswordError] = useState('');
+
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (username.length < 3 || username.length > 12) {
+      setUsernameError('닉네임은 3~12자 사이여야 합니다.');
+    } else {
+      setUsernameError('');
+    }
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(email)) {
+      setEmailError('이메일 형식이 올바르지 않습니다.');
+    } else {
+      setEmailError('');
+    }
+
+    if (
+      password.length < 6 ||
+      password.length > 16 ||
+      password === username ||
+      password === email
+    ) {
+      setPasswordError(
+        <div className={styles.spanWrapper}>
+          <span className={styles.span}>
+            비밀번호는 닉네임이나 이메일과 다르며,
+          </span>
+          <span className={styles.span}>6~16자 사이여야 합니다.</span>
+        </div>
+      );
+    } else {
+      setPasswordError('');
+    }
+
+    if (password !== confirmPassword) {
+      setConfirmPasswordError('비밀번호가 일치하지 않습니다.');
+    } else {
+      setConfirmPasswordError('');
+    }
+  }, [username, email, password, confirmPassword]);
 
   /** 회원가입 기능 수행 */
   const onSubmit = async (e) => {
     e.preventDefault();
+
+    if (
+      usernameError ||
+      emailError ||
+      passwordError ||
+      confirmPasswordError ||
+      username.length < 3 ||
+      username.length > 12 ||
+      password.length < 6 ||
+      password.length > 16 ||
+      password !== confirmPassword
+    ) {
+      return;
+    }
 
     try {
       const data = await createUserWithEmailAndPassword(auth, email, password);
@@ -26,43 +85,60 @@ function SignUpCompo() {
         // ********** [api/v1/accounts] api 완성되면 주석 제거. ********** //
         // *********************************************************** //
 
-        // const createUserResult = await fetch(`/api/api/v1/accounts`, {
-        //   method: 'POST',
-        //   body: JSON.stringify({
-        //     token,
-        //     username,
-        //   }),
-        // })
-        //   .then((res) => res.json())
-        //   .catch((err) => {
-        //     console.log({ err });
-        //     return null;
-        //   });
+        //       const createUserResult = await fetch(`/api/api/v1/accounts`, {
+        //         method: 'POST',
+        //         body: JSON.stringify({
+        //           token,
+        //           username,
+        //         }),
+        //       })
+        //         .then((res) => res.json())
+        //         .catch((err) => {
+        //           console.log({ err });
+        //           return null;
+        //         });
 
-        // if (createUserResult && !createUserResult.err) {
-        //   // 회원가입 성공
-        //   navigate('/');
-        // } else {
-        //   // 회원가입 실패
-        //   alert('회원가입 실패');
-        // }
+        //       if (createUserResult && !createUserResult.err) {
+        //         // 회원가입 성공
+        //         navigate('/');
+        //       } else {
+        //         // 회원가입 실패
+        //         alert('회원가입 실패');
+        //       }
       }
     } catch (error) {
       console.log(error.message);
-      alert('회원가입에 실패하였습니다. 새로고침 해주세요.');
+      alert('회원가입 실패, 새로고침 해주세요.');
     }
   };
-  /** 필드 입력시 해당 값 갱신 */
+
   const onChange = (e) => {
     const {
       target: { name, value },
     } = e;
 
-    if (name === 'email') {
+    if (name === 'username') {
+      setUsername(value);
+    } else if (name === 'email') {
       setEmail(value);
     } else if (name === 'password') {
       setPassword(value);
+    } else if (name === 'confirmPassword') {
+      setConfirmPassword(value);
     }
+  };
+
+  const isSignUpButtonDisabled = () => {
+    return (
+      usernameError ||
+      emailError ||
+      passwordError ||
+      confirmPasswordError ||
+      !username ||
+      !email ||
+      !password ||
+      !confirmPassword
+    );
   };
 
   return (
@@ -75,10 +151,14 @@ function SignUpCompo() {
               name="username"
               type="text"
               placeholder="닉네임"
-              // value={username}
+              value={username}
               onChange={onChange}
-              // required
+              required
+              className={styles.inputBox}
             />
+            {usernameError && (
+              <div className={styles.error}>{usernameError}</div>
+            )}
           </div>
           <div>
             <input
@@ -90,6 +170,7 @@ function SignUpCompo() {
               required
               className={styles.inputBox}
             />
+            {emailError && <div className={styles.error}>{emailError}</div>}
           </div>
           <div>
             <input
@@ -101,11 +182,29 @@ function SignUpCompo() {
               required
               className={styles.inputBox}
             />
+            {passwordError && (
+              <div className={styles.error}>{passwordError}</div>
+            )}
+          </div>
+          <div>
+            <input
+              name="confirmPassword"
+              type="password"
+              placeholder="비밀번호 확인"
+              value={confirmPassword}
+              onChange={onChange}
+              required
+              className={styles.inputBox}
+            />
+            {confirmPasswordError && (
+              <div className={styles.error}>{confirmPasswordError}</div>
+            )}
           </div>
           <div>
             <input
               type="submit"
               value="회원가입"
+              disabled={isSignUpButtonDisabled()}
               className={styles.registerBtn}
             />
           </div>
