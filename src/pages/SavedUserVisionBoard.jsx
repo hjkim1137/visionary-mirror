29c60db1774745f8548de74d33ce4f1382d87c0c
IHU <2022ihu@gmail.com>
2023-06-08 09:43:16 +0900

    [style] 코드 최신화

--- src/pages/SavedUserVisionBoard.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import SavedVisionGrid from '../components/SavedUserVisionBoard/SavedUserVisionBoard';
-
-function SavedVisionBoardGrid() {
-  return <SavedVisionGrid></SavedVisionGrid>;
-}
-
-export default SavedVisionBoardGrid;
