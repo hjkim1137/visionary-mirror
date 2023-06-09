a6017195fb0a93c8e820f2d4c7bbe952df265a11
lookethis <113021028+lookethis@users.noreply.github.com>
2023-06-09 20:54:52 +0900

    [feat] AccountsCompo get,put요청 넣(회원정보수정 취소, 탈퇴기능 없

--- src/components/Accounts/AccountsCompo.jsx
+++ src/components/Accounts/AccountsCompo.jsx
@@ -1,14 +1,11 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-// import { createUserWithEmailAndPassword } from 'firebase/auth';
-// import { auth } from '../../firebase/firebase';
-// import { updateProfile, updateEmail, updatePassword } from 'firebase/auth';
-// import { onAuthStateChanged } from 'firebase/auth';
+
 import styles from './AccountsCompo.module.scss';
 
 function AccountsCompo({ isLogin }) {
   const [formState, setFormState] = useState({
-    nickname: { value: '', valid: false, message: '', touched: false },
+    username: { value: '', valid: false, message: '', touched: false },
     email: { value: '', valid: false, message: '', touched: false },
     domain: { value: '', valid: true, message: '', touched: false },
     password: { value: '', valid: false, message: '', touched: false },
@@ -21,9 +18,43 @@ function AccountsCompo({ isLogin }) {
     navigate('/');
   }
 
-  // 기존 사용자 정보를 Firebase에서 부름
+  useEffect(() => {
+    const fetchAccountInfo = async () => {
+      try {
+        const response = await fetch('/api/v1/accounts', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          credentials: 'include',
+        });
 
-  // 입력 시 값 갱신
+        if (response.ok) {
+          const data = await response.json();
+          setFormState((prevState) => ({
+            ...prevState,
+            username: { value: data.username, valid: true, touched: true },
+            email: { value: data.email, valid: true, touched: true },
+            password: { value: '', valid: false, message: '', touched: false },
+            passwordConfirm: {
+              value: '',
+              valid: false,
+              message: '',
+              touched: false,
+            },
+          }));
+        } else {
+          throw new Error('Network response was not ok.');
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    fetchAccountInfo();
+  }, []);
+
+  // 입력 시 값 갱신, 유효성 검사
   const onChange = (e) => {
     const {
       target: { name, value },
@@ -39,7 +70,7 @@ function AccountsCompo({ isLogin }) {
       };
 
       // 닉네임이 3~12자인지 확인 (가입버튼 누르기 전)
-      if (name === 'nickname') {
+      if (name === 'username') {
         if (value.length < 3 || value.length > 12) {
           newState.message = '닉네임은 최소 3자 이상 12자 이하이어야 합니다.';
           newState.valid = false;
@@ -73,7 +104,7 @@ function AccountsCompo({ isLogin }) {
         ].filter(Boolean).length;
 
         if (
-          value.includes(formState.nickname.value) ||
+          value.includes(formState.username.value) ||
           value.includes(formState.email.value) ||
           value.length < 6 ||
           value.length > 16 ||
@@ -103,63 +134,43 @@ function AccountsCompo({ isLogin }) {
     });
   };
 
-  // onUpdate 함수 정의
-  const onUpdate = (name) => {
-    console.log('hi', name);
-    setFormState((prevState) => ({
-      ...prevState,
-      [name]: { ...prevState[name], edited: true },
-    }));
-    console.log('hi', name);
-  };
-
-  // 회원가입 기능 수행 <- 조건들
   const onSubmit = async (e) => {
     e.preventDefault();
 
-    // 각 필드의 유효성을 검사하고, 문제가 있는 필드가 있으면 알림
-    const fields = ['nickname', 'email', 'password', 'passwordConfirm'];
-    for (let field of fields) {
-      const { value, valid } = formState[field];
-      if (!valid || value === '') {
-        alert(`${field} 필드의 값이 유효하지 않습니다. 다시 확인해주세요.`);
-        return;
-      }
+    const isFormValid = Object.values(formState).every((field) => field.valid);
+    if (!isFormValid) {
+      return;
     }
 
-    // 유효성 검사 후에 실행할 코드 -> api기능 되어야 함
-    console.log('회원정보가 수정되었습니다.');
+    try {
+      const response = await fetch('/api/v1/accounts', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: formState.username.value,
+          email: formState.email.value,
+          password: formState.password.value,
+        }),
+        credentials: 'include',
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        if (data.success) {
+          console.log('회원정보가 수정되었습니다.');
+        } else {
+          throw new Error(data.message);
+        }
+      } else {
+        throw new Error('Network response was not ok.');
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
-  async function 회원정보가져오기() {
-    console.log('hello');
-    const test1234 = await fetch(`http://localhost:9999/accounts`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log('🚀 ~ file: AccountsCompo.jsx:145 ~ .then ~ data:', data);
-        // console.log(FormData);
-        setFormState((prevState) => ({
-          ...prevState,
-          nickname: { ...prevState.nickname, value: data.username },
-          email: { ...prevState.email, value: data.email },
-          password: { ...prevState.password, value: data.password },
-        }));
-      })
-      .catch((err) => {
-        console.log({ err });
-        return null;
-      });
-    console.log(test1234);
-  }
-
-  useEffect(() => {
-    회원정보가져오기();
-  }, []);
   return (
     <>
       <div className={styles.container}>
@@ -168,19 +179,16 @@ function AccountsCompo({ isLogin }) {
         <form onSubmit={onSubmit}>
           <div>
             <input
-              name="nickname"
+              name="username"
               type="text"
               placeholder="닉네임"
-              // value={username}
+              value={formState.username.value}
               onChange={onChange}
               required
               className={styles.inputBox}
             />
-            {!formState.nickname.edited && (
-              <button onClick={() => onUpdate('nickname')}>수정</button>
-            )}
           </div>
-          <div>{formState.nickname.touched && formState.nickname.message}</div>
+          <div>{formState.username.touched && formState.username.message}</div>
           <div>
             {/* {formState.nickname.edited && <div>닉네임이 수정되었습니다.</div>} */}
           </div>
@@ -190,12 +198,12 @@ function AccountsCompo({ isLogin }) {
               name="email"
               type="text"
               placeholder="이메일"
-              //value={formState.email.value}
+              value={formState.email.value}
               onChange={onChange}
               required
               className={styles.inputBox}
             />
-            @
+            {/* @
             <select name="domain" onChange={onChange} required>
               <option value="">도메인 선택</option>
               <option value="gmail.com">gmail.com</option>
@@ -204,7 +212,7 @@ function AccountsCompo({ isLogin }) {
               <option value="hanmail.net">hanmail.net</option>
               <option value="msn.com">msn.com</option>
               <option value="nate.com">nate.com</option>
-            </select>
+            </select> */}
             {/* {!formState.email.edited && (
               <button onClick={() => onUpdate('email')}>수정</button>
             )} */}
@@ -220,7 +228,7 @@ function AccountsCompo({ isLogin }) {
               name="password"
               type="password"
               placeholder="비밀번호"
-              // value={formState.password.value}
+              value={formState.password.value}
               onChange={onChange}
               required
               className={styles.inputBox}
@@ -237,7 +245,7 @@ function AccountsCompo({ isLogin }) {
               type="password"
               placeholder="비밀번호 확인"
               className={styles.inputBox}
-              // value={formState.passwordConfirm.value}
+              value={formState.passwordConfirm.value}
               onChange={onChange}
               required
             />
@@ -255,12 +263,7 @@ function AccountsCompo({ isLogin }) {
               type="submit"
               value="수정 완료하기"
               className={styles.registerBtn}
-              disabled={
-                !formState.nickname.valid ||
-                !formState.email.valid ||
-                !formState.password.valid ||
-                !formState.passwordConfirm.valid
-              }
+              disabled={!Object.values(formState).every((field) => field.valid)}
             />
           </div>
 
@@ -270,7 +273,7 @@ function AccountsCompo({ isLogin }) {
               value="수정 취소하기"
               className={styles.registerBtn}
               disabled={
-                !formState.nickname.valid ||
+                !formState.username.valid ||
                 !formState.email.valid ||
                 !formState.password.valid ||
                 !formState.passwordConfirm.valid
@@ -305,3 +308,306 @@ export default AccountsCompo;
 
 // 할 일 : 페치,기능 하나씩 추가하자.
 // 유효성 -> +get -> +put,수정 -> +취소하기 -> +탈퇴하기
+
+////////////////////////////////////
+// 밑은 파이어베이스 내장함수 사용 버전..을 쓰려고 한다
+////////////////////////////////////
+
+// import { useState, useEffect } from 'react';
+// import { useNavigate } from 'react-router-dom';
+
+// import styles from './AccountsCompo.module.scss';
+
+// function AccountsCompo({ isLogin }) {
+//   const [formState, setFormState] = useState({
+//     username: { value: '', valid: false, message: '', touched: false },
+//     email: { value: '', valid: false, message: '', touched: false },
+//     domain: { value: '', valid: true, message: '', touched: false },
+//     password: { value: '', valid: false, message: '', touched: false },
+//     passwordConfirm: { value: '', valid: false, message: '', touched: false },
+//   });
+
+//   // 로그인 되어있으면 홈('/')으로 이동
+//   const navigate = useNavigate();
+//   if (isLogin) {
+//     navigate('/');
+//   }
+
+//   useEffect(() => {
+//     const fetchAccountInfo = async () => {
+//       try {
+//         const response = await fetch('/api/v1/accounts', {
+//           method: 'GET',
+//           headers: {
+//             'Content-Type': 'application/json',
+//           },
+//           credentials: 'include',
+//         });
+
+//         if (response.ok) {
+//           const data = await response.json();
+//           setFormState((prevState) => ({
+//             ...prevState,
+//             username: { value: data.username, valid: true, touched: true },
+//             email: { value: data.email, valid: true, touched: true },
+//             password: { value: '', valid: false, message: '', touched: false },
+//             passwordConfirm: {
+//               value: '',
+//               valid: false,
+//               message: '',
+//               touched: false,
+//             },
+//           }));
+//         } else {
+//           throw new Error('Network response was not ok.');
+//         }
+//       } catch (error) {
+//         console.log(error.message);
+//       }
+//     };
+
+//     fetchAccountInfo();
+//   }, []);
+
+//   // 입력 시 값 갱신, 유효성 검사
+//   const onChange = (e) => {
+//     const {
+//       target: { name, value },
+//     } = e;
+
+//     // newState를 formState의 이전 상태를 기반으로 정의
+//     setFormState((prevState) => {
+//       const newState = {
+//         ...prevState[name],
+//         value: value,
+//         touched: true,
+//         edited: true,
+//       };
+
+//       // 닉네임이 3~12자인지 확인 (가입버튼 누르기 전)
+//       if (name === 'username') {
+//         if (value.length < 3 || value.length > 12) {
+//           newState.message = '닉네임은 최소 3자 이상 12자 이하이어야 합니다.';
+//           newState.valid = false;
+//         } else {
+//           newState.message = '유효한 닉네임입니다.';
+//           newState.valid = true;
+//         }
+//       }
+
+//       // 메일 유효성 검사 (도메인과 분리) -> 그냥 합치자
+//       if (name === 'email') {
+//         const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+//         if (!emailRegex.test(value)) {
+//           newState.message = '유효한 이메일을 입력해주세요.';
+//           newState.valid = false;
+//         } else {
+//           newState.message = '유효한 이메일입니다.';
+//           newState.valid = true;
+//         }
+//       }
+
+//       // 비밀번호에 닉네임이나 이메일 주소가 포함되어 있는지, 6~16자 이내인지 확인 (가입버튼 누르기 전)
+//       if (name === 'password') {
+//         const containsNumber = /\d/.test(value);
+//         const containsCharacter = /[a-zA-Z]/.test(value);
+//         const containsSpecialCharacter = /\W/.test(value);
+//         const countValidations = [
+//           containsNumber,
+//           containsCharacter,
+//           containsSpecialCharacter,
+//         ].filter(Boolean).length;
+
+//         if (
+//           value.includes(formState.username.value) ||
+//           value.includes(formState.email.value) ||
+//           value.length < 6 ||
+//           value.length > 16 ||
+//           countValidations < 2
+//         ) {
+//           newState.message =
+//             '비밀번호는 6자 이상 16자 이하이어야 하며, 닉네임과 이메일을 포함할 수 없습니다. 또한, 숫자, 문자, 특수 문자 중 적어도 두 가지를 포함해야 합니다.';
+//           newState.valid = false;
+//         } else {
+//           newState.message = '유효한 비밀번호입니다.';
+//           newState.valid = true;
+//         }
+//       }
+
+//       // 비밀번호 확인
+//       if (name === 'passwordConfirm') {
+//         if (value !== formState.password.value) {
+//           newState.message = '비밀번호가 일치하지 않습니다.';
+//           newState.valid = false;
+//         } else {
+//           newState.message = '비밀번호가 일치합니다.';
+//           newState.valid = true;
+//         }
+//       }
+
+//       return { ...prevState, [name]: newState };
+//     });
+//   };
+
+//   // 회원가입 기능 수행 <- 조건들
+//   const onSubmit = async (e) => {
+//     e.preventDefault();
+
+//     const isFormValid = Object.values(formState).every((field) => field.valid);
+//     if (!isFormValid) {
+//       return;
+//     }
+
+//     try {
+//       const response = await fetch('/api/v1/accounts', {
+//         method: 'PUT',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({
+//           username: formState.username.value,
+//           email: formState.email.value,
+//           password: formState.password.value,
+//         }),
+//         credentials: 'include',
+//       });
+
+//       if (response.ok) {
+//         const data = await response.json();
+//         if (data.success) {
+//           console.log('회원정보가 수정되었습니다.');
+//         } else {
+//           throw new Error(data.message);
+//         }
+//       } else {
+//         throw new Error('Network response was not ok.');
+//       }
+//     } catch (error) {
+//       console.log(error.message);
+//     }
+//   };
+
+//   return (
+//     <>
+//       <div className={styles.container}>
+//         <div className={styles.title}>마이페이지</div>
+
+//         <form onSubmit={onSubmit}>
+//           <div>
+//             <input
+//               name="username"
+//               type="text"
+//               placeholder="닉네임"
+//               value={formState.username.value}
+//               onChange={onChange}
+//               required
+//               className={styles.inputBox}
+//             />
+//           </div>
+//           <div>{formState.username.touched && formState.username.message}</div>
+//           <div>
+//             {/* {formState.nickname.edited && <div>닉네임이 수정되었습니다.</div>} */}
+//           </div>
+
+//           <div>
+//             <input
+//               name="email"
+//               type="text"
+//               placeholder="이메일"
+//               value={formState.email.value}
+//               onChange={onChange}
+//               required
+//               className={styles.inputBox}
+//             />
+//             {/* @
+//             <select name="domain" onChange={onChange} required>
+//               <option value="">도메인 선택</option>
+//               <option value="gmail.com">gmail.com</option>
+//               <option value="naver.com">naver.com</option>
+//               <option value="daum.net">daum.net</option>
+//               <option value="hanmail.net">hanmail.net</option>
+//               <option value="msn.com">msn.com</option>
+//               <option value="nate.com">nate.com</option>
+//             </select> */}
+//             {/* {!formState.email.edited && (
+//               <button onClick={() => onUpdate('email')}>수정</button>
+//             )} */}
+//           </div>
+//           <div>{formState.email.touched && formState.email.message}</div>
+
+//           <div>
+//             {/* {formState.email.edited && <div>이메일이 수정되었습니다.</div>} */}
+//           </div>
+
+//           <div>
+//             <input
+//               name="password"
+//               type="password"
+//               placeholder="비밀번호"
+//               value={formState.password.value}
+//               onChange={onChange}
+//               required
+//               className={styles.inputBox}
+//             />
+
+//             <div>
+//               {formState.password.touched && formState.password.message}
+//             </div>
+//           </div>
+
+//           <div>
+//             <input
+//               name="passwordConfirm"
+//               type="password"
+//               placeholder="비밀번호 확인"
+//               className={styles.inputBox}
+//               value={formState.passwordConfirm.value}
+//               onChange={onChange}
+//               required
+//             />
+//             {/* {!formState.password.edited && (
+//               <button onClick={() => onUpdate('password')}>수정</button>
+//             )} */}
+//           </div>
+//           <div>
+//             {formState.passwordConfirm.touched &&
+//               formState.passwordConfirm.message}
+//           </div>
+
+//           <div>
+//             <input
+//               type="submit"
+//               value="수정 완료하기"
+//               className={styles.registerBtn}
+//               disabled={!Object.values(formState).every((field) => field.valid)}
+//             />
+//           </div>
+
+//           <div>
+//             <input
+//               type="submit"
+//               value="수정 취소하기"
+//               className={styles.registerBtn}
+//               disabled={
+//                 !formState.username.valid ||
+//                 !formState.email.valid ||
+//                 !formState.password.valid ||
+//                 !formState.passwordConfirm.valid
+//               }
+//             />
+//           </div>
+
+//           <div>
+//             <input
+//               type="submit"
+//               value="탈퇴하기"
+//               className={styles.registerBtn}
+//             />
+//           </div>
+//         </form>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default AccountsCompo;
