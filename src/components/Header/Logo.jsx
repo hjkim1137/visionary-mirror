6603775c44114e7010918bcede3379de16bb45a9
IHU <2022ihu@gmail.com>
2023-06-09 20:28:43 +0900

    [fix] 코드 중첩 수정

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import styles from './Logo.module.scss';
 import logoImage from './font.png';
 
-function Logo({ to = '/', className }) {
-  return (
-    <Link to={to} className={`${styles.logoLink} ${className}`}>
-      <img src={logoImage} alt="logo" className={styles.logoImage} />
-    </Link>
-  );
+function Logo() {
+  return <img src={logoImage} alt="logo" className={styles.logoImage} />;
 }
 
 export default Logo;
