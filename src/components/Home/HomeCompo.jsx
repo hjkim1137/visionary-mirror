1f190024272f033b3958978399ff9bc854b39a87
hyunjung <paintover2023@gmail.com>
2023-06-11 12:18:47 +0900

    [design] 홈화면 디자인 수정

--- src/components/Home/HomeCompo.jsx
+++ src/components/Home/HomeCompo.jsx
@@ -3,7 +3,12 @@
 import styles from './Home.module.scss';
 
 function HomeCompo() {
-  return <div className={styles.container}>___WELCOME TO VISIONARY______</div>;
+  return (
+    <>
+      <div className={styles.container2}></div>;
+      <div className={styles.container}>___WELCOME TO VISIONARY______</div>;
+    </>
+  );
 }
 
 export default HomeCompo;
